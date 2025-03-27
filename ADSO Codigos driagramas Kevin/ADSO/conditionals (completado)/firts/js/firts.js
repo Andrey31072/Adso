52b552031f947773116408e09a1778be7c7706df
Kevin Andrey Culma Gómez <kevinandreyculmagomez@gmail.com>
2025-03-27 10:09:22 -0500

    Diagramas
    Diagramas de codigo de los ciclos, for y white, variable, condicionales.

---
+++ ADSO Codigos driagramas Kevin/ADSO/conditionals (completado)/firts/js/firts.js
@@ -0,0 +1,20 @@
+/*
+ name exercise: first
+ description: my first exercise of js with variables
+ autor: Andrey Gómez
+ Date: 15 of march of 2025
+*/
+
+let gradeOne = 4.9;
+let gradeTwo = 4.2;
+let gradeThree = 3.7; 
+
+let addition;
+
+addition = gradeOne + gradeTwo + gradeThree;
+
+let average;
+
+average = addition/3;
+
+console.log("Result average is:" + addition/3);
