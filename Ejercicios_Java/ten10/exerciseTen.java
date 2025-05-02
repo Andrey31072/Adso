c205e8a826c3f2a674eefa4ec64474f7091525c7
Kevin Andrey Culma Gómez <kevinandreyculmagomez@gmail.com>
2025-05-02 07:04:23 -0500

    Add files via upload
    Ejercicios Java del Jueves 2 de Abril del 2025 del Instructor Carlos caderna

---
+++ Ejercicios_Java/ten10/exerciseTen.java
@@ -0,0 +1,9 @@
+public class AgeToDays {
+    public static void main(String[] args) {
+        int age = 16; 
+
+        int daysLived = age * 365; 
+
+        System.out.println("You have lived approximately " + daysLived + " days (excluding leap years).");
+    }
+}
